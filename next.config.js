487e3a7f8c5dba4e317a1b18fe65b0c8f0a6bb3d
HEESUNae <heesun9456@gmail.com>
2023-04-06 21:10:25 +0900

    프로젝트 배포

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: "/NextJS-MapleStory",
   reactStrictMode: true,
-  images: {
-    domains: ["heesunae.github.io"],
-    loader: "imgix",
-    path: "http://heesunae.github.io/NextJS-MapleStory/",
-  },
 };
 
 module.exports = nextConfig;
